e28de51226ad51497c3742a711a1bd1ed9e78c13
tjacek <tjacek@student.agh.edu.pl>
2019-05-27 17:12:29 +0200

    ordered fields

--- clf_result.py
+++ clf_result.py
@@ -3,15 +3,18 @@ import itertools
 from collections import defaultdict
 
 def clf_result(in_path,out_path):
-    result=defaultdict(list)
-    for name_i,dict_i in read_results(in_path):
-    	result[name_i].append(dict_i)
-    result={ name_i:list(itertools.chain.from_iterable(value_i)) 
-                for name_i,value_i in result.items()}
+    name_dict={}#defaultdict(list)
+    data_dict=defaultdict(list)
+    for id_i,name_i,data_i in read_results(in_path):
+        name_dict[id_i]=name_i
+        data_dict[id_i].append(data_i)
+    result={ name_i:   list(itertools.chain.from_iterable(value_i)) 
+                for name_i,value_i in data_dict.items()}
+    #print(result['mhad_LR.csv'])
     make_dir(out_path)
-    for name_i,data_i in result.items():
-        f=open(out_path+'/'+name_i,'w')
-        f.write(to_csv(data_i))
+    for id_i,data_i in result.items():
+        f=open(out_path+'/'+id_i,'w')
+        f.write(to_csv(data_i,name_dict[id_i]))
         f.close()
 
 def read_results(dir_path):
@@ -26,15 +29,16 @@ def from_csv(in_path):
          lines = f.readlines()
     lines=[line_i.strip().split(',') for line_i in lines]     
     names,data= lines[0],lines[1:]
-    dicts=[{ name_j:data_j 
-    	        for name_j,data_j in zip(names,line_i)}
-    	            for line_i in data]
+    #dicts=[{ name_j:data_j 
+    #	        for name_j,data_j in zip(names,line_i)}
+    #	            for line_i in data]
     data_id=in_path.split("/")[-1]
-    return data_id,dicts
+    return data_id,names,data
 
-def to_csv( dicts):
-    names=dicts[0].keys()
-    lines=[ ",".join([ dict_i[name_j] for name_j in names])
+def to_csv( dicts,names):
+#    names=dicts[0].keys()
+    lines=[",".join(names)]
+    lines+=[ ",".join(dict_i)
                 for dict_i in dicts]
     return "\n".join(lines)
 
