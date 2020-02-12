f29bb6cdb3e66dc279a300aaef023f8b29efd234
tjacek <tjacek@agh.edu.pl>
2020-02-12 23:48:11 +0100

    show all

--- cf_matrix.py
+++ cf_matrix.py
@@ -1,9 +1,19 @@
 import numpy as np
+import os
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-def show_confusion(in_path,labels=None,title=None):       	
+def show_all(in_path,out_path):
+    all_paths=os.listdir(in_path)
+    for path_i in all_paths:
+        in_i=in_path+'/'+path_i
+        out_i=out_path+'/'+path_i
+        print(out_i)
+        show_confusion(in_path=in_i,title=path_i,out_path=out_i)
+
+def show_confusion(in_path,labels=None,title=None,out_path=None):       	
+    plt.clf()
     cf_matrix=np.genfromtxt(in_path,delimiter=',')
     dim=cf_matrix.shape
     if(not labels):
@@ -23,7 +33,11 @@ def show_confusion(in_path,labels=None,title=None):
         plt.ylabel("true labels")
     plt.yticks(rotation=0) 
     plt.xticks(rotation=90)
-    plt.show()
+    if(out_path):
+        plt.savefig(out_path)
+    else:
+        plt.show()
+
 
 cats_msr=['high arm wave','horizontal arm wave','hammer','hand catch','forward punch',
           'high throw','draw x','draw tick','draw circle','hand clap',
@@ -36,6 +50,5 @@ cats_mhad=['right arm swipe to the left', 'right arm swipe to the right', 'right
  'tennis serve', 'two hand push', 'right hand knock on door', 'right hand catch an object', 'right hand pick up and throw', 'jogging in place', 
  'walking in place', 'sit to stand', 'stand to sit','forward lunge','squat']
 
-show_confusion("conf_dtw/cf_matrix/MHAD_dtw",None,"MHAD dtw feats")
-#show_confusion("bagging/confusion_matrix/cf_mhad.txt",None)
-#show_confusion("bagging/confusion_matrix/cf_mhad_inert.txt",None)
+#show_confusion("distance_dtw/raw/skew",None,"MHAD skew")
+show_all("../ml_demo/conf_dtw/raw","../ml_demo/conf_dtw/plots")
