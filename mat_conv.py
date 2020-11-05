62d717a8ce081962af99e703354b3d353fd72e80
tjacek <tjacek@agh.edu.pl>
2020-11-05 20:37:29 +0100

    convert to math

--- mat_conv.py
+++ mat_conv.py
@@ -13,4 +13,16 @@ def read_mat(in_path,out_path):
     #print( type( data[0]))
     #print(data[0].shape)
 
-read_mat("Inertial",'inert_seqs')
+def save_mat(in_path,out_path):
+	if(not os.path.isdir(out_path)):
+		os.mkdir(out_path)
+	for name_i in os.listdir(in_path):
+		in_i="%s/%s" % (in_path,name_i)
+		out_i="%s/%s" % (out_path,name_i)
+		print(out_i)
+		print(in_i)
+		cf_i=np.genfromtxt(in_i,delimiter=',')
+		cf_i={"cf":cf_i}
+		scipy.io.savemat(out_i,cf_i)
+
+save_mat("cf",'mat')
