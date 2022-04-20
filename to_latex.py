351dfce3775bb0aa14614c501221ec3587d6dfac
tjacek <tjacek@agh.edu.pl>
2022-04-20 21:26:50 +0200

    latex

--- to_latex.py
+++ to_latex.py
@@ -1,26 +1,32 @@
 import pandas
 import os
 
-def to_latex(in_path):
-    paths=[f"{in_path}/{path_i}"
-        for path_i in os.listdir(in_path)]
-    for path_i in paths:
-        df_i=pandas.read_csv(path_i)
-        from_df(df_i)
+def to_latex(in_path,out_path):
+    if(not os.path.isdir(out_path)):
+        os.mkdir(out_path)
+    for path_i in os.listdir(in_path):
+        in_i=f"{in_path}/{path_i}"
+        out_i=f"{out_path}/{path_i}"
+        out_i=out_i.replace("csv","txt")
+        df_i=pandas.read_csv(in_i)
+        file_i = open(out_i,'w')
+        file_i.write(from_df(df_i))
+        file_i.close()
 
 def from_df(df_i):
     lines=[]
-    print(dir(df_i))
     latex="\\begin{tabular}{|"
     for t in range(df_i.shape[1]):
         latex+="c|"
     latex+="}\n"
-    
-    for index, row_j  in df_i.iterrows():
+    def helper(row_j):
         row_j=[str(elem) for elem in row_j.to_list()]
         line_j=" & ".join(row_j)
-        latex+=f"\\hline {line_j}\\\\ \n" 
+        return f"\\hline {line_j}\\\\ \n" 
+    latex+=helper(df_i.columns)
+    for index, row_j  in df_i.iterrows():
+        latex+=helper(row_j)
     latex+=" \\hline \n \\end{tabular} "
-    print(latex)
+    return latex
 
-to_latex("data")
+to_latex("data","latex")
