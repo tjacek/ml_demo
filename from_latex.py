db356607b2c2d13fc664a48c9bf26607c2dc81b6
tjacek <tjacek@agh.edu.pl>
2022-07-17 20:53:44 +0200

    from latex

---
+++ from_latex.py
@@ -0,0 +1,11 @@
+def to_csv(in_path):
+    with open(in_path) as f:
+        lines=f.readlines()
+        new_lines=[]
+        for line_i in lines:
+            line_i=line_i.replace("\\hline","")
+            line_i=line_i.replace("\\","").strip()
+            line_i= ",".join(line_i.split("&"))
+            print(line_i) 
+
+to_csv('raw')
