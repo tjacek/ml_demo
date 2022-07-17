df68f9c1a47438e29f01fd51a37c8a9d96e82f5d
tjacek <tjacek@agh.edu.pl>
2022-07-17 21:25:15 +0200

    to_latex for single files

--- to_latex.py
+++ to_latex.py
@@ -2,14 +2,17 @@ import pandas
 import os,re
 
 def to_latex(in_path,out_path):
-    if(not os.path.isdir(out_path)):
-        os.mkdir(out_path)
-    for path_i in os.listdir(in_path):
-        in_i=f"{in_path}/{path_i}"
-        out_i=f"{out_path}/{path_i}"
-        out_i=out_i.replace("csv","txt")
-        df_i=pandas.read_csv(in_i)
-        file_i = open(out_i,'w')
+    if(os.path.isdir(in_path)):
+        if(not os.path.isdir(out_path)):
+            os.mkdir(out_path)
+        for path_i in os.listdir(in_path):
+            in_i=f"{in_path}/{path_i}"
+            out_i=f"{out_path}/{path_i}"
+            to_latex(in_i,out_i)
+    else:
+        out_path=out_path.replace("csv","txt")
+        df_i=pandas.read_csv(in_path)
+        file_i = open(out_path,'w')
         file_i.write(from_df(df_i))
         file_i.close()
 
@@ -37,4 +40,4 @@ def to_biblo(in_path):
            if(not ws.match(line_j))]
     print(lines)
 
-to_biblo("bib.txt")
+to_latex("tab.csv","tab.latex")
