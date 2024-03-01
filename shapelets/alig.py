88c04ad415dbfc310df675aa85f63b31f50cd7b0
tjacek <tjacek@agh.edu.pl>
2024-03-01 19:05:04 +0100

    fix alig

--- shapelets/alig.py
+++ shapelets/alig.py
@@ -6,7 +6,7 @@ def ts(n):
 	y=[np.sin( (0.1*x_i)**2+10)/(x_i+10) for x_i in x]
 	return x,np.array(y)
 
-def shapelet(x,y,start=60,step=80):
+def shapelet(x,y,start=40,step=80):
     s_x=x[start:start+step]
     s_y=y[start:start+step]
     return s_x,1.0*s_y
@@ -20,6 +20,8 @@ def shapelet_plot(ts,s):
     ax.set_title(f'Przykładowy podciąg Shapelet')
     plt.plot(ts[0],ts[1],label="Szereg czasowy")
     plt.plot(s[0],s[1],label="Podciąg Shapelet")
+    plt.xlabel("Czas",fontsize=15) 
+    plt.ylabel("Wartość szeregu czasowego",fontsize=15) 
     plt.legend()
     plt.tight_layout()
     plt.show()
@@ -37,6 +39,8 @@ def distance_plot(ts,s):
     plt.rcParams.update({'font.size': 15})
     plt.tick_params(axis='both', which='major', labelsize=15)
     ax.set_title(f'Odległość szeregu czasowego od podciągu Shapelet')
+    plt.xlabel("Początek podciągu",fontsize=15) 
+    plt.ylabel("Odległość",fontsize=15) 
     plt.tight_layout()
     plt.plot(ts[0][:n],dist)
     plt.show()
