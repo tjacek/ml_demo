6857501f8b87078bbd1f6bb984c82aa68f772d6f
tjacek <tjacek@agh.edu.pl>
2024-06-23 15:55:23 +0200

    warp agumentation

--- agum/plot.py
+++ agum/plot.py
@@ -1,21 +1,55 @@
 import numpy as np
+#from scipy.interpolate import CubicSpline
+import scipy
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
+def get_x(start=0,end=1,n=120):
+    step= (end-start)/n
+    return np.arange(start=start,
+                     stop=end,
+                     step=step)
 
-def plot_fun(fun,
-             n_step=100, 
-             start= -35.0,
-             end= 35.0):
+def warp_agum(ts):
+    size=len(ts)
+    step=1.0/size
+    x=np.arange(start=0,
+                stop=size)/size
+    spline=scipy.interpolate.CubicSpline(x,ts)
+   
+#    quarter,eight=int(size/4),int(size/8)
+    start_x = get_x(start=0,
+                    end=0.75,
+                    n=105)
+    end_x= get_x(start=0.75,
+                 end=1.0,
+                 n=15)
+    new_x=np.concatenate([start_x,end_x])
+    new_y=spline(new_x)  
+    return get_x(n=120) ,new_y
+
+def apply_fun(fun,
+             n_step=200, 
+             start= -40.0,
+             end= 40.0):
     step=( end-start)/n_step
     x=np.array([ step*i + start for i in range(n_step)])
     ts=[fun(x_i) for x_i in x]
-    plot_ts(x,[ts])
+    return x,ts
 
-def plot_ts(x,ts):
+def plot_fun(*args, **kwargs):
+    x,ts=apply_fun(*args, **kwargs)
+    plot_ts(x,[ts],name="Szereg czasowy")
+    x,ts=warp_agum(ts)
+    x=  80*x-40
+    plot_ts(x,[ts,ts],name="Augmentowany szereg czasowy")
+
+def plot_ts(x,ts,name):
     fig, ax = plt.subplots()
     plt.figure(figsize=(10,3))
     for ts_i in ts:
         plt.plot(x,ts_i)
+    plt.title(name)
     plt.tight_layout()
     plt.show()
 
@@ -25,9 +59,5 @@ def fun(x_i):
     else:
          C=1.0/ (1.0+np.abs(x_i))
     return C*np.sin(x_i) 
-
-#def fun2(x):
-#    t=[ np.cos(x_i/3) for x_i in x]
-#    return np.array([ (t_i-5)**2 for t_i in t])
 
 plot_fun(fun)
